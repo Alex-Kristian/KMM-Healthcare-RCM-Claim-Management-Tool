8b8beb8bd27ff596a55cabca0a979e29bdc9f871
Alex-Kristian <2020alexk@gmail.com>
2026-05-14 20:17:26 -0400

    Abstract EDI parser segment handlers

--- backend/app/parsers/edi_837_parser/edi_837_parser.py
+++ backend/app/parsers/edi_837_parser/edi_837_parser.py
@@ -1,17 +1,15 @@
-from decimal import Decimal
-from datetime import datetime
+from app.parsers.edi_837_parser.edi_837_segment_handlers import *
 
 from app.parsers.edi_837_parser.edi_837_segment_handlers import extract_edi_context
 from app.models.pre_submission_claim import (
-    PreSubmissionClaim,
-    PreSubmissionService
+    PreSubmissionClaim
 )
 
 
-def finalize_claim(claim: PreSubmissionClaim, required_fields: dict, diagnosis_codes: list, claims: list, context: dict
+def finalize_claim(claim: PreSubmissionClaim, diagnosis_codes: list, claims: list, context: dict
 ):
     """
-    Finalize calculated claim fields before saving.
+    Finalize context claim fields before saving.
     """
 
     claim.payer = context["payer_name"]
@@ -19,20 +17,16 @@ def finalize_claim(claim: PreSubmissionClaim, required_fields: dict, diagnosis_c
     claim.billing_provider = context["billing_provider_name"]
     claim.payer_type = context["payer_type"]
 
-    completed = sum(required_fields.values())
-    total = len(required_fields)
-
     claim.secondary_dx_count = len(diagnosis_codes)
-
-    claim.documentation_completeness = round(
-        completed / total,
-        2
-    )
 
     claims.append(claim)
 
 
 def parse_edi_837_file(file_content: str):
+    """ 
+    Parses an EDI file and returns a list of PreSubmission Claims
+    Param: file_content:str EDI file contnents
+    """
 
     segments = [
         s.strip()
@@ -46,9 +40,6 @@ def parse_edi_837_file(file_content: str):
 
     diagnosis_codes = []
 
-    required_fields = {}
-
-
     # Extract context values that are common for all claims
     context = extract_edi_context(segments)
 
@@ -61,7 +52,6 @@ def parse_edi_837_file(file_content: str):
 
         seg_id = elements[0]
 
-
         # Start New Claim
         if seg_id == "CLM":
 
@@ -70,7 +60,6 @@ def parse_edi_837_file(file_content: str):
 
                 finalize_claim(
                     current_claim,
-                    required_fields,
                     diagnosis_codes,
                     claims,
                     context
@@ -79,35 +68,7 @@ def parse_edi_837_file(file_content: str):
             current_claim = PreSubmissionClaim()
 
             diagnosis_codes = []
-
-            required_fields = {
-              #  "billing_provider":
-              #      context["billing_provider_seen"],
-
-              #  "subscriber":
-              #      context["subscriber_seen"],
-
-              #  "payer":
-               #     context["payer_seen"],
-
-                "claim_amount": False,
-                "primary_dx": False,
-                "procedure_code": False,
-                "service_date": False,
-            }
-
-            if len(elements) > 1:
-                current_claim.claim_identifier = elements[1]
-
-            if len(elements) > 2:
-                try:
-                    current_claim.claim_amount_usd = Decimal(elements[2])
-
-                    required_fields["claim_amount"] = True
-
-                except:
-                    pass
-
+            handle_clm(elements, current_claim)
 
         # Ignore remaining segments until claim exists
         elif current_claim is None:
@@ -119,88 +80,19 @@ def parse_edi_837_file(file_content: str):
         
 
         elif seg_id == "REF":
-
-            if len(elements) > 2:
-
-                qualifier = elements[1]
-
-                # Prior authorization
-                if qualifier == "G1":
-                    current_claim.prior_auth_obtained = True
-                    current_claim.prior_auth_number = elements[2]
-
+            handle_ref(elements, current_claim)
+  
         # Service Date
         elif seg_id == "DTP":
-
-            if len(elements) > 3 and elements[1] == "434":
-
-                date_str = elements[3]
-
-                try:
-
-                    service_date = datetime.strptime(
-                        date_str,
-                        "%Y%m%d"
-                    ).date()
-
-                    current_claim.service_date = service_date
-
-                    required_fields["service_date"] = True
-
-                except:
-                    pass
-
+            handle_dtp(elements, current_claim)
 
         # Diagnosis Codes
         elif seg_id == "HI":
-
-            for value in elements[1:]:
-
-                if ":" not in value:
-                    continue
-
-                code = value.split(":")[1]
-
-                # Primary diagnosis
-                if not current_claim.primary_icd10_dx:
-
-                    current_claim.primary_icd10_dx = code
-
-                    required_fields["primary_dx"] = True
-
-                # Secondary diagnoses
-                else:
-                    diagnosis_codes.append(code)
+            handle_hi(elements, current_claim, diagnosis_codes)
 
         # Service Line
         elif (seg_id == "SV1" or seg_id == "SV2"):
-            if (seg_id == "SV1"):
-                current_claim.claim_type = "837P"
-            elif(seg_id == "SV2"):
-                current_claim.claim_type = "837I"
-
-            procedure = PreSubmissionService()
-
-            procedure.pre_submission_claim = current_claim
-
-            if len(elements) > 1:
-
-                procedure_info = elements[1]
-
-                proc_parts = procedure_info.split(":")
-
-                # HC:99213:25
-                if len(proc_parts) >= 2:
-
-                    procedure.cpt_code = proc_parts[1]
-
-                    # First modifier only
-                    if len(proc_parts) > 2:
-                        procedure.modifier = proc_parts[2]
-
-                    required_fields["procedure_code"] = True
-
-            current_claim.pre_submission_services.append(procedure)
+            handle_sv1_and_sv2(seg_id, elements, current_claim)
 
 
     # =========================================================
@@ -210,7 +102,6 @@ def parse_edi_837_file(file_content: str):
 
         finalize_claim(
             current_claim,
-            required_fields,
             diagnosis_codes,
             claims,
             context
--- backend/app/parsers/edi_837_parser/edi_837_segment_handlers.py
+++ backend/app/parsers/edi_837_parser/edi_837_segment_handlers.py
@@ -1,3 +1,12 @@
+from app.models.pre_submission_claim import PreSubmissionClaim, PreSubmissionService
+from datetime import datetime
+from decimal import Decimal
+
+
+#==================================
+# Claim Context Segment Handlers
+#==================================
+
 def extract_edi_context(segments: list[str]):
     """
     Extract shared context values from an 837 file. These values apply to multiple claims
@@ -40,78 +49,219 @@ def extract_edi_context(segments: list[str]):
 
         # ISA
         if seg_id == "ISA":
-
-            if len(elements) > 6:
-                context["sender_id"] = elements[6].strip()
-
-            if len(elements) > 8:
-                context["receiver_id"] = elements[8].strip()
-
+            handle_isa(elements, context)
         # ST
         elif seg_id == "ST":
-
-            if len(elements) > 2:
-                context["transaction_set_control"] = elements[2]
-
+            handle_st(elements, context)
 
         # Provider Specialty
         elif seg_id == "PRV":
-
-            if len(elements) > 3:
-                context["provider_specialty"] = elements[3]
-
+            handle_prv(elements, context)
 
         # Entity Segments
         elif seg_id == "NM1":
-
-            if len(elements) <= 1:
-                continue
-
-            entity = elements[1]
-
-
-            # Billing Provider
-            if entity == "85":
-
-                if len(elements) > 3:
-                    context["billing_provider_name"] = elements[3]
-
-                if len(elements) > 9:
-                    context["billing_provider_npi"] = elements[9]
-
-
-            # Subscriber
-            elif entity == "IL":
-
-                if len(elements) > 3:
-                    context["subscriber_last_name"] = elements[3]
-
-                if len(elements) > 4:
-                    context["subscriber_first_name"] = elements[4]
-
-                if len(elements) > 9:
-                    context["subscriber_id"] = elements[9]
-
-            # Payer
-            elif entity == "PR":
-
-                if len(elements) > 3:
-                    context["payer_name"] = elements[3]
-
-                    # crude payer type derivation
-                    payer_upper = elements[3].upper()
-
-                    if "MEDICARE" in payer_upper:
-                        context["payer_type"] = "Medicare"
-
-                    elif "MEDICAID" in payer_upper:
-                        context["payer_type"] = "Medicaid"
-
-                    else:
-                        context["payer_type"] = "Commercial"
-
-                if len(elements) > 9:
-                    context["payer_id"] = elements[9]
-
+            handle_nm1(elements, context)
 
     return context
+
+
+def handle_isa(elements, context):
+    """
+    Parses ISA section of an EDI file
+    Param: elements: list[str], list of ISA segment elements
+    Param: context: dict, pre submission claim context
+    """
+    if len(elements) > 6:
+        context["sender_id"] = elements[6].strip()
+
+    if len(elements) > 8:
+        context["receiver_id"] = elements[8].strip()
+
+
+def handle_st(elements, context):
+    """
+    Parses ST section of an EDI file
+    Param: elements: list[str], list of ST segment elements
+    Param: context: dict, pre submission claim context
+    """
+    if len(elements) > 2:
+        context["transaction_set_control"] = elements[2]
+
+
+def handle_prv(elements, context):
+    """
+    Parses PRV section of an EDI file
+    Param: elements: list[str], list of PRV segement elements
+    Param: context: dict, pre submission claim context
+    """
+    if len(elements) > 3:
+        context["provider_specialty"] = elements[3]
+
+
+def handle_nm1(elements, context):
+    """
+    Parses NM1 section of an EDI file
+    Param: elements: list[str], list of NM1 segment elements
+    Param: context: dict, pre submission claim context
+    """
+    if len(elements) <= 1:
+        return
+
+    entity = elements[1]
+
+    # Billing Provider
+    if entity == "85":
+
+        if len(elements) > 3:
+            context["billing_provider_name"] = elements[3]
+
+        if len(elements) > 9:
+            context["billing_provider_npi"] = elements[9]
+
+
+    # Subscriber
+    elif entity == "IL":
+
+        if len(elements) > 3:
+            context["subscriber_last_name"] = elements[3]
+
+        if len(elements) > 4:
+            context["subscriber_first_name"] = elements[4]
+
+        if len(elements) > 9:
+            context["subscriber_id"] = elements[9]
+
+    # Payer
+    elif entity == "PR":
+
+        if len(elements) > 3:
+            context["payer_name"] = elements[3]
+
+            # crude payer type derivation
+            payer_upper = elements[3].upper()
+
+            if "MEDICARE" in payer_upper:
+                context["payer_type"] = "Medicare"
+
+            elif "MEDICAID" in payer_upper:
+                context["payer_type"] = "Medicaid"
+
+            else:
+                context["payer_type"] = "Commercial"
+
+        if len(elements) > 9:
+            context["payer_id"] = elements[9]
+
+
+#=============================================
+# CLAIM SEGMENT HANDLERS
+#=============================================
+
+def handle_clm(elements, current_claim: PreSubmissionClaim):
+    """
+    Parses CLM section of an EDI file
+    Param: elements: list[str], list of CLM segment elements
+    Param: current_claim: PreSubmissionClaim, current claim being created
+    """
+    if len(elements) > 1:
+        current_claim.claim_identifier = elements[1]
+
+    if len(elements) > 2:
+        try:
+            current_claim.claim_amount_usd = Decimal(elements[2])
+
+        except:
+            pass
+
+def handle_ref(elements, current_claim: PreSubmissionClaim):
+    """
+    Parses REF section of an EDI file
+    Param: elements: list[str], list of REF segment elements
+    Param: current_claim: PreSubmissionClaim, current claim being created
+    """
+    if len(elements) > 2:
+
+        qualifier = elements[1]
+
+        # Prior authorization
+        if qualifier == "G1":
+            current_claim.prior_auth_obtained = True
+            current_claim.prior_auth_number = elements[2]
+
+
+def handle_dtp(elements, current_claim: PreSubmissionClaim):
+    """
+    Parses DTP section of an EDI file
+    Param: elements: list[str], list of CTP segment elements
+    Param: current_claim: PreSubmissionClaim, current claim being created
+    """
+    if len(elements) > 3 and elements[1] == "434":
+
+        date_str = elements[3]
+
+        try:
+
+            service_date = datetime.strptime(
+                date_str,
+                "%Y%m%d"
+            ).date()
+
+            current_claim.service_date = service_date
+
+        except:
+            pass
+
+def handle_hi(elements, current_claim: PreSubmissionClaim, diagnosis_codes:list[str]):
+    """
+    Parses HI section of an EDI file
+    Param: elements: list[str], list of HI segment elements
+    Param: current_claim: PreSubmissionClaim, current claim being created
+    Param: diagnosis_codes: list[str], list of diagnosis codes in the claim
+    """
+    for value in elements[1:]:
+
+        if ":" not in value:
+            continue
+
+        code = value.split(":")[1]
+
+        # Primary diagnosis
+        if not current_claim.primary_icd10_dx:
+
+            current_claim.primary_icd10_dx = code
+
+        # Secondary diagnoses
+        else:
+            diagnosis_codes.append(code)
+
+
+def handle_sv1_and_sv2(seg_id, elements, current_claim: PreSubmissionClaim):
+    """
+    Parses SV!or SV2 section of an EDI file
+    Param: elements: list[str], list of SV! or SV2 segment elements
+    Param: current_claim: PreSubmissionClaim, current claim being created
+    """
+    if (seg_id == "SV1"):
+        current_claim.claim_type = "837P"
+    elif(seg_id == "SV2"):
+        current_claim.claim_type = "837I"
+
+    if len(elements) > 1:
+        procedure = PreSubmissionService()
+
+        procedure.pre_submission_claim = current_claim
+
+        procedure_info = elements[1]
+
+        proc_parts = procedure_info.split(":")
+
+        if len(proc_parts) >= 2:
+
+            procedure.cpt_code = proc_parts[1]
+
+            # First modifier only
+            if len(proc_parts) > 2:
+                procedure.modifier = proc_parts[2]
+
+
+    current_claim.pre_submission_services.append(procedure)
